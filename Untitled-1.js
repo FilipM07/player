92c26924091ea27398e6106758282e043a02346d
Filip Mucha <filip.mucha@microsoft.wsei.edu.pl>
2018-10-16 17:43:05 +0200

    16.10 koniec zajęć

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,8 +1,10 @@
 let ing;
-let imgUrl;
-
+let imgUrl = "https://goo.gl/images/Wxgt51" ;
 
 let cv = Document.createElement('img');
 imgUrl.src = imgUrl
+imgUrl.addEventLidtener('load', funcion(e))
+  
 imgUrl.crossOrigin = 'Annonymous'
+
 
